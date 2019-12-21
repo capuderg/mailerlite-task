9e5dbdeecdbacbf7f6b62a581c282bc496a08636
Gregor Capuder <capuderg@gmail.com>
2019-12-21 11:51:32 +0100

    Add controller for the subscriber model

---
+++ app/Http/Controllers/SubscriberController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Subscriber;
+use Illuminate\Http\Request;
+
+class SubscriberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Subscriber::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $subscriber = Subscriber::create($request->all());
+
+        return response()->json($subscriber, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Subscriber  $subscriber
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Subscriber $subscriber)
+    {
+        return $subscriber;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Subscriber  $subscriber
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Subscriber $subscriber)
+    {
+        $subscriber->update($request->all());
+
+        return response()->json($subscriber, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Subscriber  $subscriber
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Subscriber $subscriber)
+    {
+        $subscriber->delete();
+
+        response()->json(null, 204);
+    }
+}
