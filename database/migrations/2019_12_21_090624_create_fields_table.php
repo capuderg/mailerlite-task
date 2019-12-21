a04905acc4e566a52787683b274e73562ea37343
Gregor Capuder <capuderg@gmail.com>
2019-12-21 11:19:35 +0100

    Add migrations
    for subscribers and fields tables

---
+++ database/migrations/2019_12_21_090624_create_fields_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFieldsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fields', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->tinyInteger('type');
+            $table->unsignedBigInteger('subscriber_id');
+            $table->timestamps();
+
+            $table->foreign('subscriber_id')
+                ->references('id')
+                ->on('subscribers')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fields');
+    }
+}
